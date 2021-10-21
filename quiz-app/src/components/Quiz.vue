077979e58eb1ac9913259f3d57519565a2f191c7
Jalen McGee <87670464+JalenMcG@users.noreply.github.com>
2021-10-21 18:02:28 -0400

    Update Quiz.vue

--- quiz-app/src/components/Quiz.vue
+++ quiz-app/src/components/Quiz.vue
@@ -6,7 +6,7 @@
         <ul>
           <li>
             <a class="final-link" href="https://www.aka.ms/ICWebinarOct"
-              >Webinar 1, "Advice from an Imagine Cup Competition Veteran with Dmitry Soshnikov" has already been completed. View the recording here!"</a
+              >On Demand: "Advice from an Imagine Cup Competition Veteran"</a
             >
           </li>
           <li>
